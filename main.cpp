856e61286c5a135dc0c799a6c95f988439942e45
Weier Andreas <andreas.weier.open.source@gmail.com>
2020-04-06 15:02:20 +0000

    Make exercie with minimal skelleton

--- main.cpp
+++ main.cpp
@@ -5,34 +5,19 @@
 #include <chrono> 
 #include <memory>
 
+
 /**
-Exercice 1
-----------
-Create a can device which receive
-**/ 
-// on each message received increment a counter of received message and his data type
-// create a thread which is responsible to simulate incomming messages every 0.5 seconds 
+ * Exercice 1
+ * ----------
+ * a. sent a can message with a dumy message
+ * b. simulate an incomming message every 200ms in the can device.
+ * c. every time a new message is incoming a "hello word" should be plotted out. 
+ *    Do not implent it in the simulation of the incomming message.
+ **/ 
 
-void onMsgRcv(const ICan::CanData&)
-{
-  printf("Hello World \n");
-}
-
-void simulationThread(ICan* can)
-{
-  while(true)
-  {
-    can->simulateIncommingCanMessage();
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-  }
-}
 
 int main() {
 
- std::unique_ptr<ICan> can0(std::make_unique<Can>("/dev/can0"));
- auto aThread = std::thread(simulationThread, can0.get());
- can0->registerCallback(onMsgRcv);
- can0->send({});
+ std::unique_ptr<ICan> can0(....);
 
- aThread.join();
 }
